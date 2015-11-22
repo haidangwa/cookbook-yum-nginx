7dce29ec6b9538ab73f53370cc2425177b0a07d1
St. Isidore de Seville <st.isidore.de.seville@gmail.com>
2015-11-22 13:04:16 -0500

    fix warnings

--- recipes/default.rb
+++ recipes/default.rb
@@ -28,41 +28,41 @@ node['yum-nginx']['repos'].each do |repo, value|
     #  https://github.com/chef-cookbooks/yum
     #  https://github.com/chef-cookbooks/yum/blob/master/resources/repository.rb
 
-    baseurl value['baseurl']
-    cost value['cost']
-    description value['description']
-    enabled value['enabled']
-    enablegroups value['enablegroups']
-    exclude value['exclude']
-    failovermethod value['failovermethod']
-    fastestmirror_enabled value['fastestmirror_enabled']
-    gpgcheck value['gpgcheck']
-    gpgkey value['gpgkey']
-    http_caching value['http_caching']
-    includepkgs value['includepkgs']
-    keepalive value['keepalive']
-    make_cache value['make_cache']
-    max_retries value['max_retries']
-    metadata_expire value['metadata_expire']
-    mirror_expire value['mirror_expire']
-    mirrorlist value['mirrorlist']
-    mirrorlist_expire value['mirrorlist_expire']
-    mode value['mode']
-    options value['options']
-    password value['password']
-    priority value['priority']
-    proxy value['proxy']
-    proxy_username value['proxy_username']
-    proxy_password value['proxy_password']
-    report_instanceid value['report_instanceid']
-    repositoryid value['repositoryid']
-    skip_if_unavailable value['skip_if_unavailable']
-    source value['source']
-    sslcacert value['sslcacert']
-    sslclientcert value['sslclientcert']
-    sslclientkey value['sslclientkey']
-    sslverify value['sslverify']
-    timeout value['timeout']
-    username value['username']
+    baseurl value['baseurl'] unless value['baseurl'].nil?
+    cost value['cost'] unless value['cost'].nil?
+    description value['description'] unless value['description'].nil?
+    enabled value['enabled'] unless value['enabled'].nil?
+    enablegroups value['enablegroups'] unless value['enablegroups'].nil?
+    exclude value['exclude'] unless value['exclude'].nil?
+    failovermethod value['failovermethod'] unless value['failovermethod'].nil?
+    fastestmirror_enabled value['fastestmirror_enabled'] unless value['fastestmirror_enabled'].nil?
+    gpgcheck value['gpgcheck'] unless value['gpgcheck'].nil?
+    gpgkey value['gpgkey'] unless value['gpgkey'].nil?
+    http_caching value['http_caching'] unless value['http_caching'].nil?
+    includepkgs value['includepkgs'] unless value['includepkgs'].nil?
+    keepalive value['keepalive'] unless value['keepalive'].nil?
+    make_cache value['make_cache'] unless value['make_cache'].nil?
+    max_retries value['max_retries'] unless value['max_retries'].nil?
+    metadata_expire value['metadata_expire'] unless value['metadata_expire'].nil?
+    mirror_expire value['mirror_expire'] unless value['mirror_expire'].nil?
+    mirrorlist value['mirrorlist'] unless value['mirrorlist'].nil?
+    mirrorlist_expire value['mirrorlist_expire'] unless value['mirrorlist_expire'].nil?
+    mode value['mode'] unless value['mode'].nil?
+    options value['options'] unless value['options'].nil?
+    password value['password'] unless value['password'].nil?
+    priority value['priority'] unless value['priority'].nil?
+    proxy value['proxy'] unless value['proxy'].nil?
+    proxy_username value['proxy_username'] unless value['proxy_username'].nil?
+    proxy_password value['proxy_password'] unless value['proxy_password'].nil?
+    report_instanceid value['report_instanceid'] unless value['report_instanceid'].nil?
+    repositoryid value['repositoryid'] unless value['repositoryid'].nil?
+    skip_if_unavailable value['skip_if_unavailable'] unless value['skip_if_unavailable'].nil?
+    source value['source'] unless value['source'].nil?
+    sslcacert value['sslcacert'] unless value['sslcacert'].nil?
+    sslclientcert value['sslclientcert'] unless value['sslclientcert'].nil?
+    sslclientkey value['sslclientkey'] unless value['sslclientkey'].nil?
+    sslverify value['sslverify'] unless value['sslverify'].nil?
+    timeout value['timeout'] unless value['timeout'].nil?
+    username value['username'] unless value['username'].nil?
   end if value['managed']
 end
